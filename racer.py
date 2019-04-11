5f3b4b01def514b471ae47da90be5de4dbbd7217
Vincent74230 <vincent.nowak@hotmail.fr>
2019-04-11 20:13:33 +0200

    ajout de la class Track qui sort une liste de 20 track parts, ouf..

--- racer.py
+++ racer.py
@@ -9,9 +9,17 @@ class TrackPart:
         self.length = randint (0,10)
         self.terrain = TERRAINS[randint(0,3)]
         self.complexity = COMPLEXITIES[randint(0,2)]
+    def __str__(self):
+        return "{} {} et {}".format(self.length,self.terrain,self.complexity)
 
+class Track:
+    def __init__(self):
+        self.parts = []
+        i=1
+        while i<20 :
+            (self.parts).append(TrackPart())
+            i +=1
 
-track = TrackPart()
+x = Track()
 
-
-
+print(x.parts[2])
