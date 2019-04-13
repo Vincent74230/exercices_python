c37d42fea8f90d6982eea4081cf4741a188928cd
Vincent74230 <vincent.nowak@hotmail.fr>
2019-04-13 13:33:47 +0200

    creation d'une liste de participants et affichage

--- racer.py
+++ racer.py
@@ -11,20 +11,17 @@ class TrackPart:
         self.terrain = TERRAINS[randint(0,3)]
         self.complexity = COMPLEXITIES[randint(0,2)]
     def __str__(self):
-        return "{} {} {}".format(self.length,self.terrain,self.complexity)
-  
-    
+        return "{} {} et {}".format(self.length,self.terrain,self.complexity)
+
 class Track:
     def __init__(self):
         self.parts = []
         i=0
         while i<20:
-            track = TrackPart()
-            liste_temporaire = [track.length,track.terrain,track.complexity]
-            (self.parts).append(liste_temporaire)
+            (self.parts).append(TrackPart())
             i +=1
-
-
+    def __getitem__(self,index):
+        return self.parts[index]
 
 class Pilot:
     compteur = 0
@@ -36,16 +33,36 @@ class Pilot:
         Pilot.compteur += 1
 
 class Car:
-    def __init__(self,pilot):
+    def __init__(self):
         self.name = randint(1,20)
-        self.pilot = pilot
+        self.pilot = Pilot()
         self.asphalt_speed = random()+0.5
         self.sand_speed = random()+0.5
         self.mud_speed = random()+0.5
         self.rocky_speed = random()+0.5
-  
-
-
+    def __str__(self):
+        return "car {} avec pilot {}".format(self.name,self.pilot.name)
+    #def time_for_part(self,trackPart):
+        
 
 track = Track()
-print ("Voici le circuit:"track.parts)
+i=0
+while i<20:
+    print (track[i])
+    i +=1
+
+combo_car_pilot = []
+i = 0
+while i<5:
+    nom_du_pilote = PILOT[i]
+    nom_du_pilote = Car()
+    combo_car_pilot.append(nom_du_pilote)
+    i += 1
+i=0
+while i<5:
+    print (combo_car_pilot[i])
+    i +=1
+
+
+
+
